eaa6b02127248f26d30f71752eb29d8b9830c093
Bryan Jeremias Legihono <114162318+BynJerL@users.noreply.github.com>
2023-08-30 12:00:45 +0700

    Update game_prototype.cpp
    update:
    ** adding scoring system
    ** adding "play again?"

--- game_prototype.cpp
+++ game_prototype.cpp
@@ -3,41 +3,66 @@
 #include <ctime>
 using namespace std;
 
-#define MAX_HP 9
+#define MAX_CHANCE 10
+#define MIN_NUMBER 0
+#define MAX_NUMBER 1000
 
 int main(){
     // Initialization
     srand(time(NULL));
-    int playerHP = MAX_HP;
-    int realAnswer = (rand() % 100);
-    int guessedAnswer;
     bool gameOver = false;
 
-    while(!gameOver and playerHP > 0){
-        cout << "Guess any number (0-99)" << endl;
-        cout << "Chance to guess: " << playerHP << endl;
-        cout << "Your number: "; cin >> guessedAnswer;
+    while(!gameOver){
+        int playerChance = MAX_CHANCE;
+        int realAnswer = (MIN_NUMBER + (rand() % MAX_NUMBER));
+        int guessedAnswer;
+        while(playerChance > 0){
+            cout << "Guess any number (0-999)" << endl;
+            cout << "Chance to guess: " << playerChance << endl;
+            cout << "Your number: "; cin >> guessedAnswer;
 
-        if (realAnswer == guessedAnswer) {
-            cout << "Congrats, you have guessed the right one!\n";
-            gameOver = true;
-        } else if (realAnswer != guessedAnswer) {
-            if (realAnswer > guessedAnswer) {
-                cout << "Your number is too low! ";
-                playerHP -= 1;
-            } else if (realAnswer < guessedAnswer) {
-                cout << "Your number is too high! ";
-                playerHP -= 1;
+            if (realAnswer == guessedAnswer) {
+                cout << "Congrats, you have guessed the right one!\n";
+                break;
+            } else if (realAnswer != guessedAnswer) {
+                if (realAnswer > guessedAnswer) {
+                    cout << "too low! ";
+                    playerChance -= 1;
+                } else if (realAnswer < guessedAnswer) {
+                    cout << "too high! ";
+                    playerChance -= 1;
+                } else {
+                    cout << "ERROR!\n";
+                    gameOver = true;
+                    break;
+                } 
+            }
+
+            if (playerChance > 0) {
+                cout << "Try again.\n\n";
             } else {
-                cout << "ERROR!\n";
-                gameOver = true;
-            } 
+                cout << "\nYou lose. Correct answer: " << realAnswer << "\n";
+            }
         }
 
-        if (!gameOver) {
-            cout << "Try again.\n\n";
+        cout << "Player score: " << playerChance * 100 << "\n\n";
+
+        char replayConf;
+
+        cout << "Play again? (y/n)" << endl;
+        cout << "Your answer: "; cin >> replayConf;
+
+        if(replayConf == 'y'){
+            cout << "Preparing for the next game... \n\n";
+            continue;
+        } else if(replayConf == 'n'){
+            cout << "Thanks for playing!\n"; 
+            break;
+        } else {
+            cout << "Invalid answer.\n";
+            break;
         }
     }
-
+    
     return 0;
 }
